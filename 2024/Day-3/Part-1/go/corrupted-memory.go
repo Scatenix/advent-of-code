6b5ee519169068abe0721de2d702d595be9d502a
Scatenix <38569240+Scatenix@users.noreply.github.com>
2024-12-03 17:58:59 +0100

    Go: 2024 Finished Day 3 Part 1

--- 2024/Day-3/Part-1/go/corrupted-memory.go
+++ 2024/Day-3/Part-1/go/corrupted-memory.go
@@ -5,8 +5,10 @@ import (
 	aocio "advent-of-code/aocutil/go/aoc/io"
 	aocmath "advent-of-code/aocutil/go/aoc/math"
 	aocutil "advent-of-code/aocutil/go/aoc/util"
-
+	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 const DayPart = "Day 3 - Part 1"
@@ -27,11 +29,22 @@ func main() {
 	corruptMem, err := aocio.ReadPuzzleFile[string](file, puzzleLineHandler)
 	aocutil.Check(err)
 
-	//sumMultiplications := 0;
-	print(corruptMem)
+	sumOfAllMultiplications := 0;
+	rgxMulCommand := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	// the param n means how many matches should be made at max. -1 means infinite matches are allowed.
+	mulCommands := rgxMulCommand.FindAllString(corruptMem, -1)
 
+	rgxMulNumbers := regexp.MustCompile(`\d{1,3}`)
+	for _, mul := range mulCommands {
+		rgxMulNumbers := rgxMulNumbers.FindAllString(mul, -1)
 
-	//fmt.Printf(SolutionFormat, sumMultiplications)
+		n1, err := strconv.Atoi(rgxMulNumbers[0])
+		aocutil.Check(err)
+		n2, err := strconv.Atoi(rgxMulNumbers[1])
+		aocutil.Check(err)
+
+		sumOfAllMultiplications += n1 * n2
+	}
+
+	fmt.Printf(SolutionFormat, sumOfAllMultiplications)
 }
-
-
