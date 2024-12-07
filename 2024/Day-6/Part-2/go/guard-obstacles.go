92a4621e59f93813914b7ac14bb4b99137ddb32d
Scatenix <38569240+Scatenix@users.noreply.github.com>
2024-12-07 17:59:26 +0100

    Go: 2024 Final solution for Day 6 Part 2

---
+++ 2024/Day-6/Part-2/go/guard-obstacles.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	aocio "advent-of-code/aocutil/go/aoc/io"
+	aocperf "advent-of-code/aocutil/go/aoc/perf"
+	aocslice "advent-of-code/aocutil/go/aoc/slice"
+	aocutil "advent-of-code/aocutil/go/aoc/util"
+	"atomicgo.dev/cursor"
+	"fmt"
+	"strings"
+	"time"
+)
+
+// visual only works in a terminal which can fit the whole map at once. Else there are massive bugs with it
+const visual = false
+const timeBetweenRenderInMicroSeconds = 100
+const showFromRun = 2
+
+var VisualRun = 1
+
+const DayPart = "2024 Day 6 - Part 2"
+const SolutionFormat = ">>> The solution is: %d\n"
+
+const Guard = "^"
+const Wall = "#"
+const Obstacle = "O"
+const Walkway = "."
+const PassMarker = "X"
+
+type coord struct {
+	x int
+	y int
+}
+
+// Usage: app <PATH_TO_PUZZLE_FILE>
+func main() {
+    defer aocperf.TimeTracker(time.Now(), "Main")
+    defer aocperf.PrintMemUsage(aocperf.KB, "Main")
+	puzzleFile := aocutil.AocSetup(DayPart)
+
+	puzzleLineHandler := func(line string, col [][]string) [][]string {
+		col = append(col, strings.Split(line, ""))
+		return col
+	}
+
+	guardMap, err := aocio.ReadPuzzleFile(puzzleFile, puzzleLineHandler)
+	aocutil.Check(err)
+
+	infinitumObstacle := 0
+	pos := locateStart(guardMap)
+	walkVec := coord{x: 0, y:-1}
+	dc := aocslice.DeepCopy2D(guardMap)
+
+	printMap(guardMap, pos)
+	visited := map[coord]coord{{pos.x, pos.y}: {walkVec.x, walkVec.y}}
+	_, visited = walk(dc, pos, walkVec, visited)
+
+	for place := range visited {
+		guardMap[place.y][place.x] = Obstacle
+
+		cursor.HorizontalAbsolute(0)
+		printMap(guardMap, pos)
+
+		success, _ := walk(guardMap, coord{pos.x, pos.y}, walkVec, map[coord]coord{{0, 0}: {0, 0}})
+		if !success {
+			infinitumObstacle++
+		}
+
+		guardMap[place.y][place.x] = Walkway
+	}
+	fmt.Printf(SolutionFormat, infinitumObstacle)
+}
+
+// locateStart returnx (x, y)
+// NOTE: could be done when reading the file in
+func locateStart(guardMap [][]string) coord {
+	for y := range guardMap {
+		for x := range guardMap[y] {
+			if guardMap[y][x] == Guard {
+				return coord{x: x, y: y}
+			}
+		}
+	}
+	panic("could not locate start")
+}
+
+func walk(guardMap [][]string, pos coord, vec coord, visited map[coord]coord) (bool, map[coord]coord) {
+	nextY, nextX := pos.y+vec.y, pos.x+vec.x
+
+	// infinite loop exit condition (if we are at a position we already visited, facing the same direction, we are in an infinite loop)
+	if visited[coord{nextX, nextY}] == vec {
+		return false, visited
+	}
+
+	// success exit condition
+	if pos.x == 0 || pos.y == 0 || pos.x == len(guardMap[0])-1 || pos.y == len(guardMap)-1 {
+		return true, visited
+	}
+	if guardMap[nextY][nextX] == Wall || guardMap[nextY][nextX] == Obstacle {
+		vec = turnLeft(vec)
+	} else {
+		pos.y, pos.x = nextY, nextX
+		visited[pos] = coord{vec.x, vec.y}
+	}
+
+	printPassMarker(vec)
+
+	return walk(guardMap, pos, vec, visited)
+}
+
+// The puzzle tells us to turn right, but because the map was read in backwards,
+// we also need to do everything the other way round.
+// NOTe: while it is certainly I programatic way, something like a switch case is just better for this case
+func turnLeft(vec coord) coord {
+	tmpVecX := vec.x
+	if vec.x != 0 {
+		vec.x = 0
+	} else {
+		vec.x = -vec.y
+	}
+	if vec.y != 0 {
+		vec.y = 0
+	} else {
+		vec.y = tmpVecX
+	}
+	return vec
+}
+
+
+func printMap(guardMap [][]string, pos coord) {
+	if visual {
+		VisualRun++
+		if VisualRun >= showFromRun {
+			cursor.UpAndClear(128)
+			for y := 0; y<len(guardMap); y++ {
+				for x := 0; x<len(guardMap[0]); x++ {
+					if guardMap[y][x] == Obstacle {
+						fmt.Print("\033[41m", Obstacle)
+					} else {
+						fmt.Print("\033[0m",guardMap[y][x])
+					}
+				}
+				fmt.Print("\n")
+			}
+			cursor.Move(pos.x+1, len(guardMap)-pos.y-1)
+			cursor.Hide()
+		}
+	}
+}
+
+func printPassMarker(vec coord) {
+	if visual {
+		if VisualRun >= showFromRun {
+			time.Sleep(timeBetweenRenderInMicroSeconds * time.Microsecond)
+			cursor.Move(vec.x-1, -vec.y)
+			fmt.Print("\033[46m", PassMarker)
+			fmt.Print("\033[0m", )
+		}
+	}
+}
