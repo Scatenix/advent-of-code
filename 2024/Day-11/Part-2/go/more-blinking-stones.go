d9dd02399263079b53ea100ed4e893f713a3710e
Scatenix <38569240+Scatenix@users.noreply.github.com>
2024-12-11 14:50:37 +0100

    Go: 2024 Day 11 Part 2 Finished! By adding memoization

--- 2024/Day-11/Part-2/go/more-blinking-stones.go
+++ 2024/Day-11/Part-2/go/more-blinking-stones.go
@@ -13,7 +13,7 @@ import (
 
 const DayPart = "2024 Day 11 - Part 2"
 const SolutionFormat = ">>> The solution is: %d\n"
-const Blinks = 45
+const Blinks = 75
 
 // Usage: app <PATH_TO_PUZZLE_FILE>
 func main() {
@@ -32,27 +32,31 @@ func main() {
 	stones := 0
 
 	for _, v := range puzzleInput {
-		stones += blink(v, 0)
+		stones += blink(v, 0, make(map[[2]int]int))
 	}
 
-	//fmt.Printf(SolutionFormat, puzzleInput.Len())
-	fmt.Printf("--------------")
 	fmt.Printf(SolutionFormat, stones)
 }
 
-func blink(val, blinked int) int {
+// TODO: add version without memoization to here, to be able to see how exactly it works!
+func blink(val, blinked int, memoizeMap map[[2]int]int) int {
+	if memoizeMap[[2]int{val, blinked}] != 0 {
+		return memoizeMap[[2]int{val, blinked}]
+	}
 	if blinked == Blinks {
 		return 1
 	}
 	strVal := strconv.Itoa(val)
 	if val == 0 {
-		return blink(1, blinked+1)
+		return blink(1, blinked+1, memoizeMap)
 	} else if len(strVal)%2 == 0 {
 		firstHalf, _ := strconv.Atoi(strVal[0 : len(strVal)/2])
 		secondHalf, _ := strconv.Atoi(strVal[len(strVal)/2:])
-		tmp := blink(firstHalf, blinked+1)
-		return blink(secondHalf, blinked+1) + tmp
+		tmp1 := blink(firstHalf, blinked+1, memoizeMap)
+		tmp2 := blink(secondHalf, blinked+1, memoizeMap)
+		memoizeMap[[2]int{val, blinked}] += tmp1 + tmp2
+		return tmp1 + tmp2
 	} else {
-		return blink(val*2024, blinked+1)
+		return blink(val*2024, blinked+1, memoizeMap)
 	}
 }
