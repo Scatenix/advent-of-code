80a91199e6be5a36c15922687ffa439ce67de630
Scatenix <38569240+Scatenix@users.noreply.github.com>
2024-12-14 12:00:55 +0100

    Go: 2024 Day 14 Part 1 Finished

---
+++ 2024/Day-14/Part-2/go/puzzle.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	aocio "advent-of-code/aocutil/go/aoc/io"
+	aocperf "advent-of-code/aocutil/go/aoc/perf"
+	aocutil "advent-of-code/aocutil/go/aoc/util"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/* Conclusion
+*/
+
+const DayPart = "2024 Day 14 - Part 2"
+const SolutionFormat = ">>> The solution is: %d\n"
+
+// Usage: app <PATH_TO_PUZZLE_FILE>
+func main() {
+    defer aocperf.TimeTracker(time.Now(), "Main")
+    defer aocperf.PrintMemUsage(aocperf.KB, "Main")
+	puzzleFile := aocutil.AocSetup(DayPart)
+
+	puzzleLineHandler := func(line string, col string) string {
+		col += line
+		return col
+	}
+
+	puzzleInput, err := aocio.ReadPuzzleFile(puzzleFile, puzzleLineHandler)
+	aocutil.Check(err)
+
+	solution := 0
+	for _, v := range puzzleInput {
+		num, err := strconv.Atoi(string(v))
+		aocutil.Check(err)
+		solution += num
+	}
+
+	fmt.Printf(SolutionFormat, solution)
+}
