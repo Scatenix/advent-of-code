6369a20fadb7fb78ea5e7fd264c733d348a7d03a
scatenix <38569240+Scatenix@users.noreply.github.com>
2025-11-17 19:05:06 +0100

    Go: 2019 Day 1 Part 2 Finished

---
+++ 2019/Day-1/Part-2/go/puzzle.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	aocio "advent-of-code/aocutil/go/aoc/io"
+	aocperf "advent-of-code/aocutil/go/aoc/perf"
+	aocutil "advent-of-code/aocutil/go/aoc/util"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/* Conclusion
+ */
+
+const DayPart = "2019 Day 1 - Part 2"
+const SolutionFormat = ">>> The solution is: %d\n"
+const FallbackPuzzleInputPath = "/home/sca/Programming/advent-of-code/2019/Day-1/resources/puzzle-input"
+
+// Usage: app <PATH_TO_PUZZLE_FILE>
+func main() {
+	defer aocperf.TimeTracker(time.Now(), "Main")
+	defer aocperf.PrintMemUsage(aocperf.KB, "Main")
+	puzzleFile := aocutil.AocSetup(DayPart, FallbackPuzzleInputPath)
+
+	puzzleLineHandler := func(line string, col []int) []int {
+		num, _ := strconv.Atoi(line)
+		col = append(col, num)
+		return col
+	}
+
+	puzzleInput, err := aocio.ReadPuzzleFile(puzzleFile, puzzleLineHandler)
+	aocutil.Check(err)
+
+	solution := 0
+	for _, mass := range puzzleInput {
+		fuel := mass/3 - 2
+		extraFuel := fuel
+		for extraFuel > 0 {
+			extraFuel = extraFuel/3 - 2
+			if extraFuel > 0 {
+				fuel += extraFuel
+			}
+		}
+
+		solution += fuel
+	}
+
+	fmt.Printf(SolutionFormat, solution)
+}
